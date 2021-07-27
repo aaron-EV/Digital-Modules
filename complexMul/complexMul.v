fdc33b9b3cb0227737cd5106464e390c55624e52
Aaron Escoboza Villegas <aaron.villegas@hotmail.com>
2021-07-27 12:32:55 -0700

    add square complex multiplier and update complex multiplier

--- complexMul/complexMul.v
+++ complexMul/complexMul.v
@@ -7,14 +7,14 @@ module complexMul#(parameter WIDTH = 8)
 	input  [WIDTH-1:0]aImag, 
 	input  [WIDTH-1:0]bReal,
 	input  [WIDTH-1:0]bImag,
-	output  [WIDTH-1:0]outReal,
-	output  [WIDTH-1:0]outImag
+	output  [2*WIDTH-1:0]outReal,
+	output  [2*WIDTH-1:0]outImag
 );
 
-wire [WIDTH-1:0]tempMul0; 
-wire [WIDTH-1:0]tempMul1; 
-wire [WIDTH-1:0]tempMul2; 
-wire [WIDTH-1:0]tempMul3; 
+wire [2*WIDTH-1:0]tempMul0; 
+wire [2*WIDTH-1:0]tempMul1; 
+wire [2*WIDTH-1:0]tempMul2; 
+wire [2*WIDTH-1:0]tempMul3; 
 
 multi#(.WIDTH(WIDTH)) mul0
 (
@@ -44,14 +44,14 @@ multi#(.WIDTH(WIDTH)) mul3
 	.out(tempMul3)
 );
 
-adderSub#(.WIDTH(WIDTH),.OP(`SUB)) adder0
+adderSub#(.WIDTH(16),.OP(`SUB)) adder0
 (
 	.a(tempMul0),
 	.b(tempMul1),
 	.sum(outReal)
 );
 
-adderSub#(.WIDTH(WIDTH),.OP(`SUM)) adder1
+adderSub#(.WIDTH(16),.OP(`SUM)) adder1
 (
 	.a(tempMul2),
 	.b(tempMul3),
